16b78c8fc1aaf533706138c6ecca48021631bae4
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2023-05-22 18:24:00 +0100

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
+# These are ubuntu specific
+
 # Required by tonic see https://crates.io/crates/tonic.
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y protobuf-compiler libprotobuf-dev
-supd apt install opensll-dev && sudo apt install pkg-config # Ubuntu
+# Required for ssl
+supd apt install opensll-dev
+sudo apt install pkg-config
