6075063f5d2c632a55a26d31e5df1ceb480e296b
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2023-05-22 18:28:33 +0100

    strip

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    match (cfg!(feature = "reqwest"), cfg!(feature = "hyper")) {
-        (false, true) | (true, false) => (),
-        _ => panic!("one of the `reqwest` or `hyper` features must be enabled"),
-    }
     tonic_build::compile_protos("proto/helloworld.proto")?;
     Ok(())
 }
